39aa32bd108150d57211e31de6770074c3f48ba8
victorisildur <victorisildur@163.com>
2014-09-10 21:23:59 +0800

    sample page

---
+++ CodeIgniter/application/views/fame_shop.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" href="static/css/bootstrap.min.css">
+	<link rel="stylesheet" href="static/css/shop.css">
+</head>
+<body>
+	
+	<!--顶部大图-->
+		<img src="static/img/top_pic.jpg" class="img-responsive">
+	<div class="container">
+		<!--热门-->
+		<h3>热门</h3>
+		<div class="show_box">
+			<div class="row">
+				<div class="col-xs-3 img_col img_first_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>				
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col img_last_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3 img_col img_first_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>				
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col img_last_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+			</div>
+		</div>		
+		<!--新店-->
+		<h3>新店</h3>
+		<div class="show_box">
+			<div class="row">
+				<div class="col-xs-3 img_col img_first_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p  >牛牛牛砂锅麻辣烫</p>
+				</div>
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>				
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col img_last_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3 img_col img_first_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>				
+				<div class="col-xs-3 img_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+				<div class="col-xs-3 img_col img_last_col">
+					<img src="static/img/shop.jpg" class="img-circle img-responsive shop_circle">
+					<p class="text-center"><small>牛牛牛砂锅麻辣烫</small></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
