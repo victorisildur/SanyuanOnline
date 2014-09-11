4d98aabe2f34f4937527f396f819cea10952c57c
victorisildur <victorisildur@163.com>
2014-09-11 22:51:24 +0800

    sample2

---
+++ CodeIgniter/application/views/shop_describe.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" href="../static/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../static/css/shop.css">
+</head>
+<body>
+	<div class="container">	
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">海底捞火锅</a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#">地址</a></li>												
+					</ul>
+					<ul class="nav navbar-nav navbar-left">
+						<li><a href="#">电话</a></li>						
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="../welcome">返回首页</a></li>						
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="show_box">			
+			<img src="../static/img/hotpot.jpg" class="img-responsive img-rounded">
+			<p>海底捞成功的奥秘在哪里？我认为黄铁鹰的总结重点在一段话：养而不爱如养猪，爱而不敬如养狗。而人呢，只给吃和爱是不够的，还需要尊敬。什么是对人的尊敬？见老板鞠躬给领导鼓掌？</p>
+			<p>那是对地位和权力的尊敬。对人的尊敬是信任。信任你的操守，就不会把你当贼防；信任你的能力，就会把重要的事情委托给你。</p>
+			<p>人被信任了，才会有责任感。而信任的唯一标志就是授权—海底捞给予火锅店的普通员工物质回报，还给他们“信任”与“授权”，让他们一同收获幸福感和成就感。</p>
+			
+			<img src="../static/img/hotpot.jpg" class="img-responsive img-rounded">			
+		</div>		
+		<div class="show_box">
+			<div class="btn-group btn-group-justified">
+				<div class="btn-group">
+					<button type="button" class="btn btn-default  ">
+						<img src="../static/fonts/glyphicons/png/glyphicons_001_leaf.png" > 发送给朋友
+					</button>
+				</div>
+				
+				<div class="btn-group">
+					<button type="button" class="btn btn-default ">
+						<img src="../static/fonts/glyphicons/png/glyphicons_001_leaf.png" > 分享到朋友圈
+					</button>
+				</div>
+			</div>
+			
+			<form role="form" style="margin-top:5px">
+				<div class="row">
+					<div class="col-xs-2" style="padding-right:0px">
+						<img src="../static/fonts/glyphicons/png/glyphicons_003_user.png" style="width:100%">
+					</div>
+					<div class="col-xs-10">
+						<div class="form-group">					
+							<textarea rows="3" class="form-control" id="exampleInputEmail1" placeholder="说几句吧">
+							</textarea>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-8">
+					</div>
+					<div class="col-xs-4">
+						<button type="submit" class="btn btn-info">提交</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+</body>
+</html>
