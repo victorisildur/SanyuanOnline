1d805fd5dcd3e2dca9f5f478cba6a5e64a268648
5alt <5alt>
2014-10-10 00:06:00 +0800

    bcs+ueditor
    sanyuan，只实现上传文件到bcs的功能，忽略base64和远程抓取的情况

---
+++ ueditor/php/bcs/conf.inc.php
@@ -0,0 +1,11 @@
+<?php
+//AK 公钥
+define ( 'BCS_AK', 'EiBYRpbPGY7QtZND4fUR2AY1' );
+//SK 私钥
+define ( 'BCS_SK', 'qRk8LSiNSn6oaxqZddUMIf3oOXhzPSjK' );
+//superfile 每个object分片后缀
+define ( 'BCS_SUPERFILE_POSTFIX', '_bcs_superfile_' );
+//sdk superfile分片大小 ，单位 B（字节）
+define ( 'BCS_SUPERFILE_SLICE_SIZE', 1024 * 1024 );
+//sdk superfile分片大小 ，单位 B（字节）
+define ( 'BCS_BUCKET', 'sanyuan' );
