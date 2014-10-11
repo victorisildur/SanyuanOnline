9fc6ecaf30904eeeb5bc805e9b79ddbc8423ef7c
victorisildur <victorisildur@163.com>
2014-10-11 16:00:35 +0800

    新数据表
    默认数据库名称为sanyuan

--- db.sql
+++ db.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS `shop` (
   `shop_img` varchar(255) NOT NULL,
   `shop_intro` longtext NOT NULL,
   `shop_services` varchar(255) NOT NULL,
-  `shop_status` smallint(4) NOT NULL DEFAULT 0,
+  `shop_status` tinyint(1) NOT NULL DEFAULT 0,
+  `is_old` tinyint(1) NOT NULL DEFAULT 0,
   `shop_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`shop_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -23,13 +24,21 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`comment_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
+#
+# type: 
+# 0 => 美食餐饮
+# 1 => 衣饰衣厅
+# 2 => 休闲娱乐
+# 3 => 酒店旅游
+# 4 => 更多分类
+#
 CREATE TABLE IF NOT EXISTS `coupon` (
   `coupon_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `shop_id` bigint(20) unsigned NOT NULL,
   `coupon_content` text NOT NULL,
-  `start_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `end_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `type` smallint(4) NOT NULL DEFAULT 0,
+  `start_time` date NOT NULL DEFAULT '0000-00-00',
+  `end_time` date NOT NULL DEFAULT '0000-00-00',
   `add_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`coupon_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -45,7 +54,8 @@ CREATE TABLE IF NOT EXISTS `coupon` (
 #
 CREATE TABLE IF NOT EXISTS `leaflet` (
 	`leaflet_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-	`content` longtext NOT NULL,
-	`type` smallint(4) NOT NULL,
+	`title` varchar(20) NOT NULL
+  `content` longtext NOT NULL,
+	`type` smallint(4) NOT NULL DEFAULT 0,
 	`add_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
